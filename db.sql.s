45f33e21eb2ca69ec2202cc1da0afd1a23bb9936
yeojin-206 <kimyj1692@gmail.com>
2025-04-25 10:29:17 +0900

    add product DB

--- db.sql.s
+++ db.sql.s
@@ -1,1 +1,9 @@
     password TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS product (
+    id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price TEXT NOT NULL,
+    image_url TEXT,
+    seller_id TEXT NOT NULL
+)
